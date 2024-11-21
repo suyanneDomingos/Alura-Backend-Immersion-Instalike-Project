720c5e01f827836e4a717f4949bc87af83a597b2
Charaktane <suyannegomes01@gmail.com>
2024-11-21 19:24:55 -0300

    Update postRoutes.js

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,11 +1,23 @@
 import express from 'express';
-import { listarPosts } from '../controllers/postControllers.js';
+import multer from "multer"; // Importa o Multer para lidar com uploads de arquivos
+import { listarPosts, postarNovoPost, uploadImagem } from '../controllers/postControllers.js';
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
 const routes = (app) => {
     app.use(express.json()); // Permite que o servidor receba e entenda dados no formato JSON
 
     // Rota para buscar todos os posts
     app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost)
+      app.post("/upload", upload.single("imagem"), uploadImagem); // Chama a função controladora para processamento da imagem
 }
 
 export default routes;
