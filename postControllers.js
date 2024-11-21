d6600207e951bd4620fa04a3df7d95ed3143783d
Charaktane <suyannegomes01@gmail.com>
2024-11-21 17:30:22 -0300

    Delete postControllers.js

--- postControllers.js
+++
@@ -1,7 +0,0 @@
-import { getTodosPosts } from "../models/postModel.js";
- 
-
-export async function listarPosts(req, res) {
-    const posts = await getTodosPosts(); // Busca todos os posts no banco de dados
-    res.status(200).json(posts); // Envia todos os posts como resposta em formato JSON
-} 
