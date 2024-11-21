f2b7fec0263fb9c92247036faa009dd47949c6e4
Charaktane <suyannegomes01@gmail.com>
2024-11-21 17:31:29 -0300

    Delete postModel.js

--- postModel.js
+++
@@ -1,8 +0,0 @@
-import conectarAoBanco from '../config/dbconfig.js'
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
-
-export async function getTodosPosts() {
-    const db = conexao.db('Instabytes');
-    const colecao = db.collection('posts');
-    return colecao.find().toArray();
-}
