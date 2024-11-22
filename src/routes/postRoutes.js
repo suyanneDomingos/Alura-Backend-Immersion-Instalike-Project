2e2322a79cab00f96fb86eceb27cb76c6b7bcb0d
Charaktane <suyannegomes01@gmail.com>
2024-11-22 12:23:53 -0300

    Update postRoutes.js

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -18,7 +18,7 @@ const routes = (app) => {
     app.get("/posts", listarPosts);
     app.post("/posts", postarNovoPost)
       app.post("/upload", upload.single("imagem"), uploadImagem); // Chama a função controladora para processamento da imagem
-app.put("/upload/:id", )
+app.put("/upload/:id", atualizaNovoPost)
 }
 
 export default routes;
