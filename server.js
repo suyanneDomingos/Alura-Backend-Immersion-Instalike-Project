f6cd5fc7c683a5eeac1cf4acc1737c12922e735f
Charaktane <suyannegomes01@gmail.com>
2024-11-22 12:22:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'; // Importa o framework Express para criar a aplic
 import routes from './src/routes/postRoutes.js';
 
 const app = express(); // Cria uma instância do servidor Express
+app.use(express.static("upload");
 routes(app)
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000"); // Inicia o servidor na porta 3000
