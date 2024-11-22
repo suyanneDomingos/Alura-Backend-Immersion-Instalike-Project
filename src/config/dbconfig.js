f190feca016c39e5c26e6aadde4822f4eca6faa9
Charaktane <suyannegomes01@gmail.com>
2024-11-22 19:36:45 -0300

    Update dbconfig.js

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,13 +1,17 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 export default async function conectarAoBanco(stringConexao) {
-    try {
-        const client = new MongoClient(stringConexao);
-        await client.connect();
-        console.log('Conectado ao cluster do banco de dados');
-        return client;
-    } catch (erro) {
-        console.error('Falha na conexão com o banco!', erro);
-        process.exit();
-    }
+  let mongoClient;
+
+  try {
+      mongoClient = new MongoClient(stringConexao);
+      console.log('Conectando ao cluster do banco de dados...');
+      await mongoClient.connect();
+      console.log('Conectado ao MongoDB Atlas com sucesso!');
+
+      return mongoClient;
+  } catch (erro) {
+      console.error('Falha na conexão com o banco!', erro);
+      process.exit();
+  }
 }
